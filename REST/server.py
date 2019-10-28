c0f62c9dafbf9c0762618c681c29670463015a1a
Christoffer Gustafsson <christoffer.golf@gmail.com>
2019-10-28 15:05:26 +0000

    Can now run all problems in parallel with the new run_all and status 'commands'.

--- REST/server.py
+++ REST/server.py
@@ -1,10 +1,61 @@
 from flask import Flask, request
+from celery.states import FAILURE, SUCCESS
 import subprocess
 import sys
 import tasks
 
 
 app = Flask(__name__)
+results = []
+
+
+@app.route('/benchop/status', methods=['GET'])
+def benchop_status():
+    global results
+    if len(results) == 0:
+        return "You need to run Benchop first, call benchop/run_all.\n"
+
+    # check for any fails
+    for result in results:
+        if result.state == FAILURE:
+            results = []
+            return "A task failed to complete, rerun benchop.\n"
+
+    # check for pending status
+    for result in results:
+        if result.state != SUCCESS:
+            return "Benchop is still running.\n"
+
+    # success
+    result = ""
+    try:
+        for r in results:
+            result += r.get()
+            print("got result for {}".format(r))
+    except Exception as e:
+        return "Exception [{}]\n".format(e)
+    
+    return result
+
+
+@app.route('/benchop/run_all', methods=['GET'])
+def benchop_run_all():
+    global results
+    if len(results) > 0:
+        return "Benchop is already running\n"
+    
+    try:
+        for command in tasks.problems_commands:
+            if command != "all":
+                print("queing task {}".format(command))
+                results.append(tasks.problem.delay(command))
+
+        print("queued up tasks, waiting for result")
+
+    except Exception as e:
+        result = "Problem [{}]\n".format(e)
+
+    return "Benchop is running, call benchop/status for results.\n"
 
 
 @app.route('/benchop/run', methods=['GET'])
